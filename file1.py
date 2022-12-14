60662afd30ac5a5e9a29273ebb1cad2ad818dab9
enterme00 <95276537+enterme00@users.noreply.github.com>
2022-12-14 18:58:09 +0300

    шаблон с ошибкой

--- file1.py
+++ file1.py
@@ -1,11 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask (__name__)
-@app.route ("/index")
 @app.route ("/")
 def index():
-    return "index"
+    return render_template('D:\кп бд\kp_bd\kp_bd\templates\index.html')
 @app.route("/about")
 def about():
-    return "<h1> О сайте </h1>"
+    return "<h1> одно окно </h1>"
 if __name__ == "__main__":
     app.run(debug=True)
