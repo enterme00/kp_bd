7c55e9f79e30cf91c0fc1d5cfa27d4e62c367920
enterme00 <95276537+enterme00@users.noreply.github.com>
2022-12-14 20:33:21 +0300

    3

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
 app = Flask (__name__)
+menu = ["Время и режим работы", "Полезная информация", "Объявления", "Общая информация"]
 @app.route ("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title="Главная ", menu=menu)
 @app.route("/about")
 def about():
-    return "<h1> одно окно </h1>"
+    return render_template('about.html', title = "О сайте", menu = menu)
 if __name__ == "__main__":
     app.run(debug=True)
