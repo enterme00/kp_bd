d4bf34564029bc312f8873e5b5da8930b5edb19d
enterme00 <95276537+enterme00@users.noreply.github.com>
2022-12-15 20:09:42 +0300

    Не работает линк на обратную связь

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from flask import Flask, render_template, url_for
 app = Flask (__name__)
 
-@app.route ("/")
+@app.route ("/index")
 def index():
     print(url_for('index'))
-    return render_template('index.html', title="Главная ")
+    return render_template('index.html', title="Главная")
 @app.route("/about")
 def about():
     print(url_for('about'))
     return render_template('about.html', title = "О сайте")
+    @app.route("/connect")
+    def about():
+        print(url_for('connect'))
+        return render_template('connect.html', title = "О сайте")
 @app.route("/profile/<username>")
 def profile(username):
     return f"Пользователь:{username}"
