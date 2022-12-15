0574004aece8bad4508a19301347c704b8f82205
enterme00 <95276537+enterme00@users.noreply.github.com>
2022-12-15 14:48:53 +0300

    ОШИБКА! class button. Кнопка не открывает меню в мобильной версии

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask (__name__)
-menu = ["Время и режим работы", "Полезная информация", "Объявления", "Общая информация"]
+
 @app.route ("/")
 def index():
-    return render_template('index.html', title="Главная ", menu=menu)
+    print(url_for('index'))
+    return render_template('index.html', title="Главная ")
 @app.route("/about")
 def about():
-    return render_template('about.html', title = "О сайте", menu = menu)
+    print(url_for('about'))
+    return render_template('about.html', title = "О сайте")
+@app.route("/profile/<username>")
+def profile(username):
+    return f"Пользователь:{username}"
 if __name__ == "__main__":
     app.run(debug=True)
