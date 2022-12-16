5ecf522338a6f6c2566a86beea68b53413f9c7ca
enterme00 <95276537+enterme00@users.noreply.github.com>
2022-12-16 12:32:40 +0300

    (Connect) Перемистьтить чуть ближе к центру?????

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ def index():
 def about():
     print(url_for('about'))
     return render_template('about.html', title = "О сайте")
-    @app.route("/connect")
-    def about():
-        print(url_for('connect'))
-        return render_template('connect.html', title = "О сайте")
+@app.route("/connect")
+def connect():
+    print(url_for('connect'))
+    return render_template('connect.html', title = "Обратная связь")
 @app.route("/profile/<username>")
 def profile(username):
     return f"Пользователь:{username}"
