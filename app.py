34e0ea8b73b73081cb67c1b528c540b05d362724
enterme00 <95276537+enterme00@users.noreply.github.com>
2022-12-17 20:46:33 +0300

    ОШибка!!! Не переносит на страницу с проФИЛЕМ

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, flash
+from flask import Flask, render_template, url_for, session,redirect,request, flash
 app = Flask (__name__)
 app.secret_key = "secret_key"
 @app.route ("/index")
@@ -24,5 +24,19 @@ def connect():
 @app.errorhandler(404)
 def pageNotFount(error):
     return render_template('404.html', title="Страница не найдена")
+
+
+@app.route("/profile/<username>")
+def profile(username):
+    return f"Профиль пользователя:{username}"
+@app.route("/login", methods=["POST", "GET"])
+def login():
+     if 'userLogged' in session:
+         return redirect(url_for('profile',username=session['userLogged']))
+     elif request.method=='POST' and request.form['username']== "suicidalsun@gmail.com" and request.form['psw']=="123":
+         session['userLogged']=request.form['username']
+         return redirect(url_for('profile',username=session['userLogged']))
+     return render_template('login.html', title="Авторизация")
+
 if __name__ == "__main__":
     app.run(debug=True)
