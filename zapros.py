bdf87fb37a8adbe36b1bf7f66de6e80bca4766b3
Андрей <goster5@yandex.ru>
2022-12-23 08:45:31 +0300

    Check

--- zapros.py
+++ zapros.py
@@ -4,8 +4,8 @@ from playhouse.shortcuts import model_to_dict, dict_to_model
 def sotrudnikVxod(email,parol):
     try:
         sotrudnik = Sotrudnik.select()
-            .where(sotrudnik.email == 'email', sotrudnik.parol =='password')
+            .where(email == email, parol == password)
             .get()
-        return model_to_dict (userLogged)
+        return model_to_dict(sotrudnik)
     except:
         return False
