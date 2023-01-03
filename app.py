f3ef668038b1098ce72aa6bf1674dea945391490
RFfront <goster5@yandex.ru>
2023-01-04 00:48:54 +0300

    priem

--- app.py
+++ app.py
@@ -5,10 +5,12 @@ from peewee import *
 from playhouse.db_url import connect
 from playhouse.mysql_ext import MySQLConnectorDatabase
 from zapros import *
+from datetime import datetime
 app = Flask(__name__)
 app.secret_key = "secret_key"
 
 
+@app.route("/")
 @app.route("/index")
 def index():
     print(url_for('index'))
@@ -38,11 +40,12 @@ def connect():
 
 @app.route('/login', methods=["POST", "GET"])
 def login():
+    if session.get('sotrudniki') != None:
+        return redirect('/sotrudnik')
+
     if request.method != 'POST':
         return render_template('login.html')
 
-    if session.get('sotrudniki') != None:
-        return redirect('/sotrudnik')
     res = request.form
     sotrudniki = sotrudnikVxod(res.get('email'), res.get('parol'))
     if sotrudniki:
@@ -80,21 +83,53 @@ def obyav():
     return render_template('obyav.html', title="Объявления:")
 
 
+@app.route('/startPriem', methods=['POST'])
+def startPriem():
+    print(request.form)
+    try:
+        dt = datetime.fromisoformat(request.form.get('datetime'))
+    except:
+        return redirect('/priem')
+    gra = Gragdanin.create(**(request.form))
+    yav = request.form.get('yavka')
+    if yav == 'false':
+        yav = False
+    else:
+        yav = True
+    # print(dt.date(),gra.idgragdanin,request.form.get('procedure'))
+    pr = Priem.create(
+        data=dt.date(),
+        id_gragdanin=gra.idgragdanin,
+        id_prosedura=request.form.get('procedure'),
+        id_sotrudnik=session.get('sotrudniki')['idsotrudnik'],
+        time=dt.time(),
+        yavka=yav
+    )
+    return redirect(f'/priem2/{pr.idpriem}')
+
+
 @app.route('/priem')
 def priem():
-    print(url_for('priem'))
-    return render_template('priem.html', title="Заполните данные:")
+
+    return render_template('priem.html', title="Заполните данные:", procedures=Procedura.select())
 
 
-@app.route('/priem2', methods=["POST", "GET"])
-def priem2():
+@app.route('/endPriem', methods=["POST"])
+def endPriem():
+    prid = request.form['prid']
+    Zapros.create(id_priem=request.form['prid'], organistiya_idorganistiya=request.form['org'],
+                  text=request.form['text'], data=request.form['date'])
+    return redirect(f'/priem2/{prid}')
+
+
+@app.route('/priem2/<priemid>', methods=["POST", "GET"])
+def priem2(priemid):
     if request.method == 'POST':
         if len(request.form.get("sms")) > 10:
             flash("Запрос отправлен успешно.", category='success')
         else:
             flash("Ошибка отправки. Повторите попытку", category='error')
-    print(url_for('priem2'))
-    return render_template('priem2.html')
+    return render_template('priem2.html', title="Заполните данные:", prid=priemid, organiz=Organistiya.select(),zapr=Zapros.select().where(Zapros.id_priem ==priemid))
 
 
 if __name__ == "__main__":
